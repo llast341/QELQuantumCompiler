3e865440428ae0e4e34ea61af7ec846944258ad6
llast341 <lsteiner@sreflect.de>
2024-05-29 15:50:24 +0200

    Starting to build a simple Quantum Circuit

--- test.py
+++ test.py
@@ -1,45 +1,23 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit.circuit.library import HGate
+import matplotlib.pyplot as plt
+import numpy as np
 
-# python program to create a simple Quantum circuit 
-  
-  
+qc = QuantumCircuit(2,2)
+qc.x(0)
+qc.h(1)
+qc.swap(0, 1)
+qc.h(0)
+print(qc.data)
+qc.draw("mpl")
+qc.data[1].operation.definition.draw('mpl')
+qc.data[2].operation.definition.draw('mpl')
 
-from qiskit import QuantumCircuit, execute, Aer, IBMQ 
-from qiskit.compiler import transpile, assemble 
-from qiskit.tools.jupyter import *
-from qiskit.visualization import *
-  
-# Loading your IBM Q account(s) 
-provider = IBMQ.load_account() 
-   
-# Create a Quantum Circuit acting 
-# on the q register 
-circuit = QuantumCircuit(2, 2) 
-   
-# Add a H gate on qubit 0 
-circuit.h(0) 
-   
-# Add a CX (CNOT) gate on control  
-# qubit 0 and target qubit 1 
-circuit.cx(0, 1) 
-   
-# Map the quantum measurement to the 
-# classical bits 
-circuit.measure([0,1], [0,1]) 
-   
-# Use Aer's qasm_simulator 
-simulator = Aer.get_backend('qasm_simulator') 
-   
-# Execute the circuit on the qasm 
-# simulator 
-job = execute(circuit, simulator, shots=1000) 
-   
-# Grab results from the job 
-result = job.result() 
-   
-# Return counts 
-counts = result.get_counts(circuit) 
-print("\nTotal count for 00 and 11 are:",counts) 
-  
-# Draw the circuit 
-circuit.draw()  
-#hello
+qc2 = QuantumCircuit(1)
+qc2.append(
+    HGate(),
+    [0]
+)
+qc2.draw("mpl")
+
+plt.show()
